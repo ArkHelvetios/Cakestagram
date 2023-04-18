b0ba848ff2c59e23045d79035f8f9981d33b253d
ArkHelvetios <arkhelvetios@gmail.com>
2023-04-19 00:04:44 +0300

    Homework 2 - create photos data

---
+++ js/utility.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const getRandomNum = (min = 0, max = 100) => {
+  let result = Math.floor(Math.random() * (max - min + 1) + min);
+
+  if ((min < 0 || max < 0) || (min == max)) return -1;
+  if (min > max) [min, max] = [max, min];
+
+  return result
+};
+
+const checkMessageLength = (message = '', maxLength = 180) => {
+  return message.length <= maxLength;
+}
+
+export {
+  getRandomNum,
+  checkMessageLength
+}
